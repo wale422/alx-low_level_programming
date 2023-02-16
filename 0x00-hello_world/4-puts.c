b3d407c8f95e9ea61a7bd8f9bb5bc4615ad29f5a
wale422 <waletole@gmail.com>
2023-02-16 09:24:55 -0800

    print using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program to print a line of text with puts
+ * Return:0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
