7568c05775248320efce6c66ba7ee5d8a5db3bfa
wale422 <waletole@gmail.com>
2023-02-16 09:29:51 -0800

    print using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program to print a line of text with puts
+ * Return:0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
