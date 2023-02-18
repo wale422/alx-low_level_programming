f439f486c5c7b9c5d6207e08c4195f7ed58b40aa
wale422 <waletole@gmail.com>
2023-02-17 23:49:59 -0800

    triple combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print lower case alphabet in reverse
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i <= 9; i++)
+	{
+		for (j = i ; j <= 9; j++)
+			for (k = j ; k <= 9; k++)
+		{
+			if (i != j != k)
+			{	putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
+				if (i == 7 && j == 8 && k == 9)
+					continue;
+				else
+					putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
