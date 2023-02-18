e75acb00ae32bea9a211323395ba290d469f8857
wale422 <waletole@gmail.com>
2023-02-17 22:18:53 -0800

    combination

---
+++ 0x01-variables_if_else_while/100-print_comb2.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print lower case alphabet in reverse
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i <= 9; i++)
+	{
+		putchar('0' + i);
+		for (j = i; j < i; j++)
+		putchar('0' + j);
+		if (i == 9 || j == 9)
+			continue;
+		else
+			putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
