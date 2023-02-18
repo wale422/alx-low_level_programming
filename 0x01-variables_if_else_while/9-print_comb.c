edb4e1ab51bf004b028663099ca1148e763b0bc6
wale422 <waletole@gmail.com>
2023-02-17 21:47:29 -0800

    singel digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print lower case alphabet in reverse
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0 ; num <= 9; num++)
+		putchar('0' + ',' + 'num);
+	putchar('\n');
+	return (0);
+}
