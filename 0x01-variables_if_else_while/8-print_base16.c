27d1341117e70df63e033f935586d7cdceb77ae6
wale422 <waletole@gmail.com>
2023-02-17 21:31:26 -0800

    base 16 num

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print lower case alphabet in reverse
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar('0' + num);
+	for (c = 'a' ; c <= 'e'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
