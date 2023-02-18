5e5d3b10601b5ae34a28f5aeaff59790c2e597f4
wale422 <waletole@gmail.com>
2023-02-17 17:41:03 -0800

    a-z in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebalpha.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print lower case alphabet in reverse
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z' ; c > 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
