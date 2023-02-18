60af2bfbf9f3bc871e4cc3d58b8c969fe3319b38
wale422 <waletole@gmail.com>
2023-02-17 22:36:19 -0800

    combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,12 +12,15 @@ int main(void)
 	int j;
 
 	for (i = 0 ; i <= 9; i++)
-		for (j = i; j <= 9; j++)
-			putchar('0' + ij);
+		for (j = i; j <= i; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
 			if (i == 9 || j == 9)
 				continue;
 			else
 				putchar(',');
+		}
 	putchar(' ');
 	putchar('\n');
 	return (0);
