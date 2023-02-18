194b6fa2e9389fe8f0f2898c4304de0abb216ff2
wale422 <waletole@gmail.com>
2023-02-18 08:56:41 -0800

    comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print lower case alphabet in reverse
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i <= 9; i++)
+	{
+		for (j = 0 ; j <= 9; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			if (i == 9 && j == 9)
+				continue;
+			else
+				putchar(',');
+			putchar(' ');
+			
+		}
+	}
+	putchar('\n');
+	return (0);
+}
