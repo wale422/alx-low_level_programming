d8228ae9f7722131fa2efaf0c72c668425a89e8b
wale422 <waletole@gmail.com>
2023-02-22 13:00:47 -0800

    sum of even terms less then 400000

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,17 +10,20 @@ int main(void)
 	unsigned long int first_term = 1;
 	unsigned long int second_term = 2;
 	unsigned long int next_term = 0;
+	unsigned long int max_term = 4000000;
+	unsigned long int sum = 0;
 
 	for (i = 0 ; i < n; i++)
 	{
-		if (first_term % 2 == 0)
+		if ((first_term % 2 == 0) && (first_term < max_term))
 		{
-			printf("%lu, ", first_term);
+			sum += first_term;
 		}
 		next_term  = first_term + second_term;
 		first_term = second_term;
 		second_term = next_term;
 	}
+	printf("%lu", sum);
 	printf("\n");
 	return (0);
 }
