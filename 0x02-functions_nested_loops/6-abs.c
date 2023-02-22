b1de9c8c85c1d8d6409831fe3eb2e53a98b2fc8a
wale422 <waletole@gmail.com>
2023-02-21 22:02:57 -0800

    abs value of int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _abs -computes absolute value of integer
+* @n: number to be checked for sign
+* Return: absolute value of n
+*/
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
