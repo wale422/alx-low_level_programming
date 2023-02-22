f419031e449a680c19ecaf316ac2cd56bd5a2a6c
wale422 <waletole@gmail.com>
2023-02-22 10:04:50 -0800

    multiple of 3 or 5 below1024

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
 * main - compute multiples of 3 or 5 below 1024
-* Return : returns the sum .
+* Return : nothing.
 */
 
 int  main(void)
 {
 	int i;
 	int sum;
-    
+
 	sum = 0;
 	for (i = 0; i < 1024; i++)
 	{
