e96163f5476c6cc0b759d02a549400ebe08112cc
wale422 <waletole@gmail.com>
2023-02-22 13:04:35 -0800

    first 100 terms

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+  * main - print first 98 fibonacci numbers.
+  * Return: nothing.
+  */
+int main(void)
+{
+	int n = 98;
+	int i;
+	unsigned long int first_term = 1;
+	unsigned long int second_term = 2;
+	unsigned long int next_term = 0;
+
+	for (i = 0 ; i < n; i++)
+	{
+		printf("%lu", first_term);
+		if (!(i == (n - 1)))
+		{
+			printf(", ");
+		}
+		next_term  = first_term + second_term;
+		first_term = second_term;
+		second_term = next_term;
+	}
+	printf("\n");
+	return (0);
+}
+
