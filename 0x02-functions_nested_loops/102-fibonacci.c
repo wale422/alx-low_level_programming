5975bd4dff7dd8a90adf30583161a558e505e850
wale422 <waletole@gmail.com>
2023-02-22 12:05:55 -0800

    first 50 fib

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+  * main - print first 50 fibonacci numbers.
+  * Return: nothing.
+  */
+int main(void)
+{
+	int n = 50;
+	unsigned long int first_term = 1;
+	unsigned long int second_term = 2;
+	unsigned long int next_term = 0;
+	
+
+	while (n > 0 )
+	{
+		printf("%lu, \n",first_term);
+		next_term  = first_term + second_term;
+		first_term = second_term;
+		second_term = next_term;
+		n--;
+	}
+	printf("\n");
+}
+
