234837f9f650a619aa5bd3781b922fb466cfd214
wale422 <waletole@gmail.com>
2023-02-22 21:40:03 -0800

    remaining days

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,8 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int , int , int );
+int largest_number(int, int, int );
+int convert_day(int, int );
+void print_remaining_days(int, int, int );
 
 #endif
