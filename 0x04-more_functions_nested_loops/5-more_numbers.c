8952e58104bb7ecd059a0093428ac70f01467cfe
wale422 <waletole@gmail.com>
2023-02-23 18:27:37 -0800

    pront 0-14 *10

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * more_numbers - Print 0 up to 14 10 times
+  *
+  * Return:0-14 * 10
+  */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+			}
+
+			_putchar((y % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
+}
