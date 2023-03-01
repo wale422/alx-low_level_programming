958ed75f0f4befd6677a3e02cde408065be19439
wale422 <waletole@gmail.com>
2023-02-28 17:33:21 -0800

    print every other

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,12 +7,16 @@
   */
 void puts2(char *str)
 {
+	int i;
 	int j = 0;
 
 	while (str[j] != '\0')
 	{
-		_putchar(str[j]);
-		j += 2;
+		j++;
+	}
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
