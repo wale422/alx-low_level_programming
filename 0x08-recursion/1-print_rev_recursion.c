70215c042a4ab949ccbab9c1c0036bf8015b08a5
wale422 <waletole@gmail.com>
2023-03-08 15:37:39 -0800

    print string in revverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: the string
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
