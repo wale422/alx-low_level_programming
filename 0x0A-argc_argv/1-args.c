cc3ae194271ea3beea5747d3621618ca019b535d
wale422 <waletole@gmail.com>
2023-03-10 12:55:01 -0800

    arg count

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - prints the number of arguments
+ * main - prints the name of the executable file
  * @argc: argument count
  * @argv: pointer to arguments passed
  *
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, __attribute__ ((unused)) char *argv[])
 {
 	printf("%d\n", argc);
 	return (0);
