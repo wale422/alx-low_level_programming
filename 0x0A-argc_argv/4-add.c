5bd13fa5b4582097476f7000e472d54bee7c7908
wale422 <waletole@gmail.com>
2023-03-11 10:49:21 -0800

    add args

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,18 +19,16 @@ int main(int argc, char *argv[])
 	}
 	for (i = 1; i < argc; i++)
 	{
-		for(j = 0; argv[i][j] !='\0'; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!isdigit(argv[i][j]))
+			if (!(isdigit(argv[i][j])))
 			{
 				printf("Error\n");
 				return (1);
 			}
-		
 		}
 		sum += atoi(argv[i]);
 	}
-	
 	printf("%d\n", sum);
 	return (0);
 }
